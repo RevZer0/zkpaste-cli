37553331c822f298fc1e19a1e444c31115404f26
RevZer0 <6ke6rn6@gmail.com>
2025-06-20 12:46:33 +0300

    feat(paste): encrypt and commit paste

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/RevZer0/zkpaste-cli/handler"
+	"github.com/RevZer0/zkpaste-cli/service"
+	"github.com/RevZer0/zkpaste-cli/utils"
 )
 
 func main() {
-	fmt.Println("Initial commit")
+	test_encrypt()
+}
+
+func test_encrypt() {
+	message := "This is password protected paste"
+	password := "12345"
+
+	ciphertext, iv, key, signature := service.EncryptPaste(message, password)
+
+	pasteId := handler.CreatePasteHandler(
+		utils.ArmorValue(ciphertext),
+		utils.ArmorValue(iv),
+		utils.ArmorValue(signature),
+	)
+	fmt.Println("http://localhost:3000/paste/" + pasteId + "#" + utils.ArmorValue(key))
 }
